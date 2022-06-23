e38e5c8df200f2e0e96314b511a8bb862e4395fd
Manuel Pineda <pineda.manuel@gmail.com>
2022-06-23 12:22:16 -0500

    Connection with RabitMQ for configuration refresh and api-Gateway included

---
+++ api-gateway/src/test/java/com/sofkaU/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
